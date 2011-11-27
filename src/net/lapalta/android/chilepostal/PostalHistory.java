97d7648a90057a82122a68d0bcd633ed0ca70b3f
Dušan Juretić <dusan.juretic@gmail.com>
2011-11-27 00:25:50 -0300

    Removed history.xml, cleaned up ChilePostal.java

--- src/net/lapalta/android/chilepostal/PostalHistory.java
+++ src/net/lapalta/android/chilepostal/PostalHistory.java
@@ -1,6 +1,5 @@
 package net.lapalta.android.chilepostal;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.app.ListActivity;
@@ -9,7 +8,6 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.provider.Contacts.Phones;
 import android.text.ClipboardManager;
 import android.view.ContextMenu;
 import android.view.Menu;
@@ -17,11 +15,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.CursorAdapter;
 import android.widget.ListAdapter;
 import android.widget.SimpleCursorAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
@@ -37,19 +32,7 @@ public class PostalHistory extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //this.setContentView(android.R.layout.simple_list_item_1);
         
-        //this.setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new String[] {"hola", "chao"}));
-        // Get a cursor with all phones
-        //Cursor c = getContentResolver().query(Phones.CONTENT_URI, null, null, null, null);
-        //startManagingCursor(c);
-        
-        // Map Cursor columns to views defined in simple_list_item_2.xml
-       /* ListAdapter adapter = new SimpleCursorAdapter(this,
-                android.R.layout.two_line_list_item, c, 
-                        new String[] { Phones.NAME, Phones.NUMBER }, 
-                        new int[] { android.R.id.text1, android.R.id.text2 });
-        setListAdapter(adapter);*/
         initDB();
         fillList();
         registerForContextMenu(this.getListView());
@@ -74,15 +57,11 @@ public class PostalHistory extends ListActivity {
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,
         ContextMenuInfo menuInfo) {
- //     if (v.getId()==R.id.text2||v.getId()==R.id.text1) {
-        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
-
         menu.setHeaderTitle("Opciones");
         menu.add(Menu.NONE, 0, 0, "Copiar código postal");
         menu.add(Menu.NONE, 1, 1, "Copiar dirección");
         menu.add(Menu.NONE, 2, 2, "Borrar ítem");
         
- //     }
     }
 
 	
@@ -90,16 +69,11 @@ public class PostalHistory extends ListActivity {
     public boolean onContextItemSelected(MenuItem item) {
 	    AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
 	    int menuItemIndex = item.getItemId();
-	    Intent index2 = item.getIntent();
 	    
 	    ClipboardManager cM = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-	    TextView tv = null;
 	    if (menuItemIndex==1) {
-	    	//ListAdapter ad = (ListAdapter) this.getListAdapter().getItem(menuItemIndex);
 	    	showNotif("Copiada dirección: "+item_direccion.get(info.position));
 	    	cM.setText(item_direccion.get(info.position));
-	    	//showNotif("Selected item: "+info.position + "\nDireccion: "+ item_direccion.get(info.position) + "\nPostal: "+item_postal.get(info.position));
-	    	//item.getItemId();
 	    }
 	    if (menuItemIndex==0) {
 	    	showNotif("Copiado código postal: "+item_postal.get(info.position));
@@ -123,19 +97,13 @@ public class PostalHistory extends ListActivity {
         } catch (SQLException e) {
         	showNotif(e.toString());
         }
-        /*ContentValues cv = new ContentValues();
-        cv.put("direccion", "PRUEBA");
-        cv.put("codigo_postal", 254243);
-		db.insert(this.DB_TABLE, null, cv);*/
 	}
 	
 	public void deleteFromDatabase(String direccion, int postal) {
 		initDB();
 		if (db!=null) {
 			// TODO: make safe input
-			//db.rawQuery("DELETE FROM codigos WHERE direccion = '"+direccion+ "' AND codigo_postal = '"+postal+"';",null);
 			db.execSQL("DELETE FROM codigos WHERE direccion = '"+direccion+ "' AND codigo_postal = '"+postal+"';");
-			//db.delete("codigos", "direccion = '"+direccion+"'", whereArgs)
 			fillList();
 		}
 	}
@@ -153,15 +121,8 @@ public class PostalHistory extends ListActivity {
 		int i = 0;
 		item_postal.clear();
 		item_direccion.clear();
-		//c = db.rawQuery("SELECT _id, direccion, codigo_postal FROM codigos ORDER BY _id DESC;", null);
-		//c.requery();
 		if (c!=null&&c.getCount()>0) {
-			//CursorAdapter ca = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item, c, null, null);
 			do {
-				
-				//int curr = c.getInt(2);
-
-				//ca.getItem(c.getPosition());
 				item_postal.put(i, c.getInt(c.getColumnIndex("codigo_postal")));
 				item_direccion.put(i, c.getString(c.getColumnIndex("direccion")));
 				i++;
@@ -178,8 +139,6 @@ public class PostalHistory extends ListActivity {
 		);
 
 		this.setListAdapter(adapter);
-		//View vi = (View) adapter.getItem(0);
-		//c.close();
 	}
 	
 	public void showNotif(String msg) {
